48ab8808d27f41e7a4ab021017ff0a3e369fc009
toannh-0136 <nguyen.huy.toan@sun-asterisk.com>
2019-10-15 09:17:25 +0700

    Bug#1-Fix-bugs

--- js/master.js
+++ js/master.js
@@ -1,13 +1,39 @@
-$('.js-search-button').click(function () {
-  $(this).children().toggleClass('fa-times').toggleClass('fa-search');
-  $(this).prev().toggle();
+window.onscroll = function(){
+  var y = window.scrollY;
+
+  if(y > 0){
+    $('.js-header').addClass('header--stuck');
+    $('.js-nav-children').addClass('nav__children--stuck');
+  } else {
+    $('.js-header').removeClass('header--stuck');
+    $('.js-nav-children').removeClass('nav__children--stuck');
+  }
+};
+
+$('.js-search-button').click(function (e) {
+  var inputContainer = $(this).prev();
+
+  if(inputContainer.hasClass('form-search__hidden')){
+    e.preventDefault();
+    inputContainer.removeClass('form-search__hidden');
+    inputContainer.find('.form-search__input').focus();
+  }
+});
+
+$('.js-close-button').click(function (e) {
+  var inputContainer = $(this).parents('.form-search__input-container');
+
+  inputContainer.addClass('form-search__hidden');
 });
 
 $('.js-review-carousel').slick({
+  lazyLoad: 'progressive',
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
   autoplay: true,
+  fade: true,
+  speed: 600,
   autoplaySpeed: 8000,
   dots: true,
   appendDots: $('.js-review-control'),
@@ -17,8 +43,9 @@ $('.js-review-carousel').slick({
   }
 });
 
-function switchTab(nextTab){
+function switchTab(nextTab) {
   var idNextTab = $(nextTab).attr('data-tab');
+
   $('.js-tab [data-tab]').removeClass('tab__active');
   $(nextTab).addClass('tab__active');
   $('.carousel-product__main').hide();
@@ -27,8 +54,8 @@ function switchTab(nextTab){
   $(`#${idNextTab} .js-carousel-product`).slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 10000,
+    autoplay: true,
+    autoplaySpeed: 3000,
     nextArrow: $(`#${idNextTab} .control-next`),
     prevArrow: $(`#${idNextTab} .control-prev`)
   });
@@ -36,7 +63,7 @@ function switchTab(nextTab){
 
 $('.js-tab [data-tab]').click(function (e) {
   var idCurrentTab = $('.js-tab .tab__active').attr('data-tab');
-  console.log(idCurrentTab);
+
   $(`#${idCurrentTab} .js-carousel-product`).slick('unslick');
   e.preventDefault();
   switchTab(this);
